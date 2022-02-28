53e1953808854018080b124cc6fe054470d93b09
Eduardo Rivero <lalo@lalorivero.com>
2022-02-27 21:57:35 -0700

    Character class that describes the data from the API

---
+++ lib/characters/model/character.dart
@@ -0,0 +1,28 @@
+/// This class describes the data that will be use from rickandmortyapi
+
+class Character {
+  final String id, name, status, species, image;
+  final Map origin, location;
+
+  const Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.origin,
+    required this.location,
+    required this.image,
+  });
+
+  factory Character.fromJson(Map<String, dynamic> json){
+    return Character(
+        id: json['id'].toString(),
+        name: json['name'],
+        status: json['status'],
+        species: json['species'],
+        origin: json['origin']['name'],
+        location: json['location']['name'],
+        image: json['image']
+    );
+  }
+}
