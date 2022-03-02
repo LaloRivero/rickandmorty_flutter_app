4df57f14743925ec00e640ebbf3e25387cace4ea
Eduardo Rivero <lalo@lalorivero.com>
2022-03-01 21:51:26 -0700

    Gender data add it to the character model

--- lib/characters/model/character.dart
+++ lib/characters/model/character.dart
@@ -1,13 +1,14 @@
 /// This class describes the data that will be use from rickandmortyapi
 
 class Character {
-  final String id, name, status, species, image, firstSeen, location;
+  final String id, name, status, species, gender, image, firstSeen, location;
 
   const Character({
     required this.id,
     required this.name,
     required this.status,
     required this.species,
+    required this.gender,
     required this.firstSeen,
     required this.location,
     required this.image,
@@ -19,6 +20,7 @@ class Character {
         name: json['name'],
         status: json['status'],
         species: json['species'],
+        gender: json['gender'],
         firstSeen: json['firstSeen'],
         location: json['location']['name'],
         image: json['image']
