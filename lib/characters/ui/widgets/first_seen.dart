2c3599102f2321a3539c7ff915593a79f4cac033
Eduardo Rivero <lalo@lalorivero.com>
2022-03-01 21:52:02 -0700

    First seen widget

---
+++ lib/characters/ui/widgets/first_seen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FirstSeen extends StatelessWidget{
+  const FirstSeen ({Key? key, this.textSize, this.character}) : super(key: key);
+  final textSize;
+  final character;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+            margin: const EdgeInsets.only(
+              top: 10,
+              bottom: 5,
+            ),
+            child:  Text(
+              "First seen in:",
+              style: TextStyle(
+                fontSize: textSize,
+                color: Color.fromARGB(255, 158, 158, 158),
+              ),
+            )),
+        Text(
+          character.firstSeen,
+          style: TextStyle(fontSize: textSize),
+        )
+      ],
+    );
+  }
+}
